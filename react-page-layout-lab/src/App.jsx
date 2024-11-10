2d391358ae86a4086e561c983e65a8373b1161e0
Prameela Pydi <nprameela.pydi@gmail.com>
2024-11-09 23:18:19 -0800

    initial page setup

---
+++ react-page-layout-lab/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header.jsx'
+import HomePage from './components/HomePage.jsx'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HomePage />
+    </>
+  )
+}
+
+export default App
